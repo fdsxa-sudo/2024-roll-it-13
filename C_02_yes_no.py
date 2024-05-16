079aa1f59785758fdfa7c8e80a6eabc65579d763
fdsxa-sudo <148297594+fdsxa-sudo@users.noreply.github.com>
2024-05-17 10:31:38 +1200

    Fixed yes no error msg, created int checker > 13

--- C_02_yes_no.py
+++ C_02_yes_no.py
@@ -8,10 +8,13 @@ def yes_no(question):
         elif response == "no" or response == "n":
             return "no"
         else:
-            print("you did not choose a valid response ")
+            print("you did not choose a valid response")
 
 
 # main questions
 while True:
-    want_instructions = yes_no("Do you want to read the instructions? ")
+    want_instructions = yes_no("Do you want to read the instructions?")
     print(f"you chose {want_instructions}")
+
+    roll_again = yes_no("Do yo want to roll again")
+    print(f"You said {roll_again}")
