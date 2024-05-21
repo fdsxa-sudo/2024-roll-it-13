9196414ae85cc79c50db46af16f089d4718564a6
fdsxa-sudo <148297594+fdsxa-sudo@users.noreply.github.com>
2024-05-21 12:40:54 +1200

    Partially working rounds

--- C_02_yes_no.py
+++ C_02_yes_no.py
@@ -16,5 +16,5 @@ while True:
     want_instructions = yes_no("Do you want to read the instructions?")
     print(f"you chose {want_instructions}")
 
-    roll_again = yes_no("Do yo want to roll again")
-    print(f"You said {roll_again}")
+    roll_again = yes_no("Do you want to roll again")
+    print(f"You chose {roll_again}")
